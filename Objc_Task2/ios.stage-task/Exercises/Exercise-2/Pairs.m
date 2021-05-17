70341044849bfe8f97266c2b7f19ae517ca700a1
rgn-coder <reginadryagina@gmail.com>
2021-05-17 13:03:25 +0300

    feat: Task2 was completed

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int count = 0;
+    for (int a = 1; a < array.count; a += 1) {
+        for (int i = array.count - 2; i >= 0; i -= 1) {
+            if (array[array.count - a].intValue - array[i].intValue == number.intValue) {
+                count += 1;
+            }
+        }
+    }
+    return count;
 }
 
 @end
